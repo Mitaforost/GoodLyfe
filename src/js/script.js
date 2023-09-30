7700dde80abb1a3d41651a744873a97371287da2
danila <zaharchuk.danila18@gmail.com>
2023-09-30 14:26:57 +0300

    next

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,17 @@
-$(document).ready(function (){
-    $('.carousel__inner').slick(
-        {
-            speed: 1200,
-            adaptiveHeight: true,
-            responsive: [
-                {
-                    breakpoint: 992,
-                    settings: {
-                        dots: true,
-                        arrows: false
-                    }
-                }
-            ]
-        }
-    )
-})
+// $(document).ready(function (){
+//     $('.carousel__inner').slick(
+//         {
+//             speed: 1200,
+//             adaptiveHeight: true,
+//             responsive: [
+//                 {
+//                     breakpoint: 992,
+//                     settings: {
+//                         dots: true,
+//                         arrows: false
+//                     }
+//                 }
+//             ]
+//         }
+//     )
+// })
