45965cbd663bc5e96eed942e8802139d220f6565
danila <zaharchuk.danila18@gmail.com>
2023-10-01 17:51:55 +0300

    sport/meet сделано на половину

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,8 +3,8 @@ $(document).ready(function (){
         {
             arrows: false,
             dots: true,
-            slidesToShow: 1,
-            slidesToScroll: 1,
+            // slidesToShow: 1,
+            // slidesToScroll: 1,
             autoplay: true,
             autoplaySpeed: 5000,
         }
