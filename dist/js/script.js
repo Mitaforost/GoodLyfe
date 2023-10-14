65e7afbcadee91153d26ea0f39d08a6bf4f88d27
danila <zaharchuk.danila18@gmail.com>
2023-10-14 16:43:15 +0300

    carousel nev

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,10 +1,16 @@
-$(document).ready(function (){
-    $('.carousel__inner').slick(
-        {
-            arrows: false,
-            dots: true,
-            autoplay: true,
-            autoplaySpeed: 5000,
-        }
-    )
-})
+// $(document).ready(function (){
+//     $('.carousel__inner').slick(
+//         {
+//             arrows: false,
+//             dots: true,
+//             autoplay: true,
+//             autoplaySpeed: 5000,
+//         }
+//     )
+// })
+$(".carousel__inner").slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+});
