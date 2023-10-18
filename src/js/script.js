86143ff9e97e644c42208724d2fca4ff0f0b8926
danila <zaharchuk.danila18@gmail.com>
2023-10-18 18:31:05 +0300

    burger

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,13 @@ $(document).ready(function () {
         autoplaySpeed: 5000,
     });
 });
+const hamburger = document.querySelector('.header__hamburger'),
+    menu = document.querySelector('.header__active'),
+    closeElement = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () =>{
+    menu.classList.add('active');
+});
+closeElement.addEventListener('click', () =>{
+    menu.classList.remove('active');
+});
